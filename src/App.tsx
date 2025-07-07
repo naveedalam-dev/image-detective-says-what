799e7e04fc4a252620a97512866f0992ca9c44e1
Naveed Alam <62477022+naveedalam-dev@users.noreply.github.com>
2025-07-07 14:04:43 +0500

    Remove index page and start from login

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ const App = () => (
         <Sonner />
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<Index />} />
+            <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/dashboard/*" element={<Dashboard />}>
